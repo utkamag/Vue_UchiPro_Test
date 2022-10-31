dc1fa063ecdfd815f9eeb8533d9d40889d40fdbc
utkamag <92789705+utkamag@users.noreply.github.com>
2022-10-31 15:26:34 +0400

    update

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   devServer: {
-    proxy: 'https://vuekez.herokuapp.com/',
+    proxy: 'https://vuekez.herokuapp.com/tokens',
   }
-})
+}
